0ef967b2d65bfc9a6f0db7bdae372e1a6313117f
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-05 18:09:31 +0900

    Update thumnail.py

--- thumnail.py
+++ thumnail.py
@@ -2,4 +2,9 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 
 im = img.imread('표지.png')
-plt.imshow(im)
+plt.imshow(im
+           
+# import PIL.Image as pilimg
+#
+# a = pilimg.open('./표지.png')
+# a     
