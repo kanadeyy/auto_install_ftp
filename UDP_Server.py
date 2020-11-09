3be8db175f254c20d803b820853b9ac1a56d7961
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-09 21:52:27 +0900

    Update UDP_Server.py

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,38 +1,25 @@
 #파일을 받음
 import socket
-import time
-import threading
 import getipmac
+import time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+def send_data(ID):
+    IP_address, mymac = getipmac.getipmac()
+    PC_ID = ID
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server.bind(('', 8371))
-# server.settimeout(0.2)
+    sock.bind(('',8731))
+    data,addr = sock.recvfrom(1024)
+    check,Serverip = data.decode('utf-8').split(",")
 
-IP_address, mac_address = getipmac.getipmac()
-PC_ID = 'PC1' # PC IP를 입력받는 부분/ 임시데이터
-message = IP_address+','+PC_ID
-print(message)
+    sock.close()
 
-# order, add = server.recvfrom(2048)
-# if( ):
+    if check == "RIS":
+        time.sleep(2)
+        sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        message = IP_address + "," + PC_ID
+        sock2.sendto(message.encode('utf-8'),(Serverip,8732))
+        print('데이터 보냄')
+        sock2.close()
 
-
-while True:
-    # 클라이언트에서 특정 메세지를 받음
-    # data, addr = server.recvfrom(2048)
-    # print('key: ', data.decode())
-
-    # 만약 그 데이터가 맞다면
-    # if data != 0:
-    server.sendto(message.encode('utf-8'), ('<broadcast>', 8371))
-    print("send message")
-    data,addr = server.recvfrom(1024)
-    time.sleep(3)
-    # if data == message.decode():
-    #     break
-    # else :
-    #     print('특정 메세지 수신을 기다리는중')
-    #     time.sleep(1)
+send_data('PC1')
