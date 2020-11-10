6d38ebe7c90cc272a8c22aaf5a5a76faf3922161
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-10 19:56:45 +0900

    Add files via upload

--- Client/receivelink.py
+++ Client/receivelink.py
@@ -2,12 +2,10 @@ import socket
 import pickle
 
 def receivelink(serverip):
-    serverip = '192.168.0.6'
-    list = []
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((serverip,8733))
     data, addr = sock.recvfrom(4096)
-    list = pickle.loads(data)
-    print(list)
+    down_list = pickle.loads(data)
+    sock.close()
+    return down_list
 
-    return list
