7bf4eee444e5a312de2f252b878a2cbc76252bae
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-17 23:21:29 +0900

    Update Client_UI.py

--- test_jbw/Client/Client_UI.py
+++ test_jbw/Client/Client_UI.py
@@ -4,15 +4,16 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import getipmac
-from Client import PC_ID
-from Client import UDP_Server
-from Client import receivelink
-from Client import ftp_client
+import PC_ID
+import UDP_Server
+import receivelink
+import ftp_client
 import threading
-
+import time
 
 form_class = uic.loadUiType("Client_UI.ui")[0]
-
+global Serverip
+Serverip = "null"
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
@@ -37,6 +38,7 @@ class WindowClass(QMainWindow, form_class):
         #트레이아이콘
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(QIcon('icon.png'))
+        self.tray_icon.setToolTip("RIS Client")
         show_action = QAction("정보 보기", self)
         quit_action = QAction("종료", self)
         show_action.triggered.connect(self.show)
@@ -66,22 +68,50 @@ class WindowClass(QMainWindow, form_class):
         self.hide()
 
 
-def protocol():
-    ftp_client.makeAppdir()
-    ID = PC_ID.file_read()
-    Serverip = UDP_Server.send_data(ID)
-    traynotification = WindowClass()
+def udpcommunicationforever():
+    global Serverip
     while True:
-        recv_link = receivelink.receivelink(Serverip)
-        traynotification.notify("RIS Client","설치 시작")
-        ftp_client.FTP_download(Serverip,recv_link)
-        traynotification.notify("RIS Client", "설치 완료")
+        Serverip = UDP_Server.send_data()
+
+def udp_recv_ftp_download():
+    global recv_link
+    recv_link = []
+    while True:
+        if Serverip != "null":
+            recv_link.append(receivelink.receivelink(pcip))
+        else:
+            pass
+
+def ftp_downforever():
+    global recv_link
+    while True:
+        try:
+            ftp_link = recv_link.pop(0)
+            traynotification = WindowClass()
+            traynotification.notify("RIS Client", "설치 시작")
+            ftp_client.FTP_download(Serverip, ftp_link)
+            traynotification.notify("RIS Client", "설치 완료")
+            time.sleep(5)
+        except:
+            pass
 
 if __name__ == "__main__":
+    global pcip
+    pcip, pcmac = getipmac.getipmac()
+    ftp_client.makeAppdir()
 
-    srv = threading.Thread(target=protocol)
+    srv = threading.Thread(target=udpcommunicationforever)
     srv.daemon = True
+
+    srv2 = threading.Thread(target=udp_recv_ftp_download)
+    srv2.daemon = True
+
+    srv3 = threading.Thread(target=ftp_downforever)
+    srv3.daemon = True
+
     srv.start()
+    srv2.start()
+    srv3.start()
 
     # QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv)
