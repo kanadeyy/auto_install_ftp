a4b1e8687b9df27384f03a3c995d85f0fb0742a8
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-17 23:23:41 +0900

    Update UDP_Server.py

--- test_jbw/Client/UDP_Server.py
+++ test_jbw/Client/UDP_Server.py
@@ -1,24 +1,24 @@
 import socket
 import getipmac
 import time
+import PC_ID
 
-def send_data(ID): # 서버로 부터 RIS 메시지 수신시 자신의 IP와 PC의 ID 송신
+def send_data(): # 서버로 부터 RIS 메시지 수신시 자신의 IP와 PC의 ID 송신
     IP_address, mymac = getipmac.getipmac()
-    PC_ID = ID
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     sock.bind(('',8731))
     data,addr = sock.recvfrom(1024)
-    check,Serverip = data.decode('utf-8').split(",")
+    check,Server_ip = data.decode('utf-8').split(",")
 
     sock.close()
 
     if check == "RIS":
         time.sleep(2)
         sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        message = IP_address + "," + PC_ID
-        sock2.sendto(message.encode('utf-8'),(Serverip,8732))
-        print('데이터 보냄')
+        PCID = PC_ID.file_read()
+        message = IP_address + "," + PCID
+        sock2.sendto(message.encode('utf-8'),(Server_ip,8732))
         sock2.close()
 
-    return Serverip
+    return Server_ip
