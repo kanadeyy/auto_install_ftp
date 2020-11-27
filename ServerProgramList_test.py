0166781c45b428115dc04823103872cced11d2e0
kanadeyy <50947678+kanadeyy@users.noreply.github.com>
2020-11-27 22:25:19 +0900

    Update ServerProgramList_test.py

--- ServerProgramList_test.py
+++ ServerProgramList_test.py
@@ -11,9 +11,85 @@ import ftp_client
 import threading
 import time
 
-form_class = uic.loadUiType("ServerProgramList.ui")[0]
+form_class1 = uic.loadUiType("Client_UI.ui")[0]
+form_class2 = uic.loadUiType("ServerProgramList.ui")[0]
 
-class WindowClass1(QWidget, form_class):
+global Serverip
+Serverip = "null"
+
+class WindowClass(QMainWindow, form_class1):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # 배경만들기
+        image = QImage("thumbnail.png")
+        palette = QPalette()
+        palette.setBrush(10, QBrush(image))
+        self.setPalette(palette)
+
+        # Close Button 이미지 만들기
+        self.CloseButton.setStyleSheet('image:url(Close.png);border:0px;')
+
+        #ID,IP주소 리스트에 저장
+        ID = PC_ID.file_read()
+        IP,MAC = getipmac.getipmac()
+        self.iptext.append(IP)
+        self.idtext.append(ID)
+
+        ID_ = self.IP_Change_Edit.text()
+
+        # 트리거 만들기
+        self.okbutton.clicked.connect(self.okbuttonFunction)
+        self.IP_Change_Edit.returnPressed.connect(self.okbuttonFunction)
+        self.CloseButton.clicked.connect(self.CloseButtonFunction)
+
+        # 제목표시줄 없애기
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        #트레이아이콘
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon('icon.png'))
+        self.tray_icon.setToolTip("RIS Client")
+        show_action = QAction("정보 보기", self)
+        quit_action = QAction("종료", self)
+        show_action.triggered.connect(self.show)
+        quit_action.triggered.connect(qApp.quit)
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        trayDouble = self.tray_icon.activated
+        trayDouble.connect(self.trayDoubleclick)
+        self.tray_icon.show()
+
+    def trayDoubleclick(self, reason):
+        if reason == 2:
+            self.main = Widgets()
+            self.main.show()
+
+    def notify(self, title, message):
+        self.tray_icon.showMessage(title, message, 1, 3000)
+
+    def CloseButtonFunction(self):
+        self.hide()
+
+    def okbuttonFunction(self):
+        ID_ = self.IP_Change_Edit.text()
+        if not ID_:
+            QMessageBox.warning(self,"Remote Install Software","메세지를 입력하세요") #https://pythonprogramminglanguage.com/pyqt5-message-box/
+
+        else :
+            PC_ID.file_modify(ID_)
+            self.idtext.clear()
+            self.IP_Change_Edit.clear()
+            self.idtext.append(ID_)
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+
+class Widgets(QWidget, form_class2):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -30,12 +106,12 @@ class WindowClass1(QWidget, form_class):
         # Close Button 이미지 만들기
         self.CloseButton.setStyleSheet('image:url(Close.png);border:0px;')
 
-        #트리거 생성
-        # self.ReceiveButton.clicked.connect(self.ReceiveButtonFunction)
-        self.CloseButton.clicked.connect(self.CloseButtonFunction)
-
-        #UI에 프로그램 리스트 추가
+        #프로그램 리스트 추가
         self.addProgram()
+
+        #트리거 생성
+        self.ReceiveButton.clicked.connect(self.ReceiveButtonFunction)
+        self.CloseButton.clicked.connect(self.CloseButtonFunction)
 
     def CloseButtonFunction(self):
         self.hide()
@@ -45,7 +121,7 @@ class WindowClass1(QWidget, form_class):
         if not selected_Item:
             QMessageBox.warning(self, "Remote Install Software", "내용을 선택하세요")
 
-        else :
+        else:
             self.hide()
             self.tray_icon.show()
             SendProgram = []
@@ -59,19 +135,66 @@ class WindowClass1(QWidget, form_class):
 
     def addProgram(self):
         # z, x, y = filelist.duplecheck()
-        x = ['ex1','ex2','ex3','ex4']
+        x = ['ex1', 'ex2', 'ex3', 'ex4']
         for i in x:
             self.ProgramList.addItem(i)
 
+
+def udpcommunicationforever():
+    global Serverip
+    while True:
+        Serverip = UDP_Server.send_data()
+
+def udp_recv_ftp_download():
+    global recv_link
+    recv_link = []
+    while True:
+        if Serverip != "null":
+            recv_link.append(receivelink.receivelink(pcip))
+        else:
+            pass
+
+def ftp_downforever():
+    global recv_link
+    while True:
+        try:
+            ftp_link = recv_link.pop(0)
+            traynotification = WindowClass()
+            traynotification.notify("RIS Client", "설치 시작")
+            ftp_client.FTP_download(Serverip, ftp_link)
+            traynotification.notify("RIS Client", "설치 완료")
+            time.sleep(5)
+        except:
+            pass
+
 if __name__ == "__main__":
+    global pcip
+    pcip, pcmac = getipmac.getipmac()
+    ftp_client.makeAppdir()
+
+    srv = threading.Thread(target=udpcommunicationforever)
+    srv.daemon = True
+
+    srv2 = threading.Thread(target=udp_recv_ftp_download)
+    srv2.daemon = True
+
+    srv3 = threading.Thread(target=ftp_downforever)
+    srv3.daemon = True
+
+    srv.start()
+    srv2.start()
+    srv3.start()
+
     # QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv)
 
-    # WindowClass의 인스턴스 생성
-    myWindow = WindowClass1()
+    #인스턴스 생성
+    myWindow = WindowClass()
+    myWidget = Widgets()
 
     # 프로그램 화면을 보여주는 코드
-    myWindow.show()
+    #myWindow.show()
+    #myWidget.show()
 
     # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
     app.exec_()
